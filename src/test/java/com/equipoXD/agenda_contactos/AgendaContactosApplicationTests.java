932aff6a04952e186da43d3fc58a75870b64804b
Luis_Ang <158246858+Luisangelriverosbautista@users.noreply.github.com>
2025-05-07 15:11:44 -0600

    first commit

---
+++ src/test/java/com/equipoXD/agenda_contactos/AgendaContactosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipoXD.agenda_contactos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaContactosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
