ed343b9a84ee5a6b9c2be3ccaacc00a68606d168
Luis_Ang <158246858+Luisangelriverosbautista@users.noreply.github.com>
2025-05-07 18:48:33 -0600

    act.5 creando metodo post en el controller

--- src/main/java/com/equipoXD/agenda_contactos/controller/agendaContactosController.java
+++ src/main/java/com/equipoXD/agenda_contactos/controller/agendaContactosController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.equipoXD.agenda_contactos.model.agendaContactosModel;
@@ -14,8 +16,14 @@ import com.equipoXD.agenda_contactos.repository.agendaContactosRepository;
 public class agendaContactosController {
     @Autowired
    private agendaContactosRepository agendaContactosRepository;
+   //metodo para traer contactos
    @GetMapping("/traerContactos")
    public List <agendaContactosModel> traerContactos() {
        return agendaContactosRepository.findAll();
     }
+    //metodo para agragar contactos a la base de datos
+   @PostMapping("/agregarContacto")
+   public agendaContactosModel agregarContacto(@RequestBody agendaContactosModel contacto) {
+       return agendaContactosRepository.save(contacto);
+   }
 }
