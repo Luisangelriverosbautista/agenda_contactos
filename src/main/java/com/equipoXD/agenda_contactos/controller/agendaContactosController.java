da710d6c1b6f662d552821435c2184c448399751
Luis_Ang <158246858+Luisangelriverosbautista@users.noreply.github.com>
2025-05-08 15:18:48 -0600

    actividad 6 agregando el metodo put en el controlador

--- src/main/java/com/equipoXD/agenda_contactos/controller/agendaContactosController.java
+++ src/main/java/com/equipoXD/agenda_contactos/controller/agendaContactosController.java
@@ -2,9 +2,13 @@ package com.equipoXD.agenda_contactos.controller;
 
 import java.util.List;
 
+import org.hibernate.query.NativeQuery.ReturnProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +30,17 @@ public class agendaContactosController {
    public agendaContactosModel agregarContacto(@RequestBody agendaContactosModel contacto) {
        return agendaContactosRepository.save(contacto);
    }
+   @PutMapping("/editarContacto/{id}")
+    public ResponseEntity<agendaContactosModel> editarContacto (@PathVariable Long id, @RequestBody agendaContactosModel contacto) {
+       return agendaContactosRepository.findById(id).map(contactoExistente -> {
+           contactoExistente.setNombre(contacto.getNombre());
+           contactoExistente.setTelefono(contacto.getTelefono());
+           contactoExistente.setEmail(contacto.getEmail());
+           contactoExistente.setDireccion(contacto.getDireccion());
+           contactoExistente.setOficio(contacto.getOficio());
+           contactoExistente.setImagen(contacto.getImagen());
+           agendaContactosModel actualizado = agendaContactosRepository.save(contactoExistente);
+           return ResponseEntity.ok(actualizado);
+       }).orElse(ResponseEntity.notFound().build());
+    }
 }
