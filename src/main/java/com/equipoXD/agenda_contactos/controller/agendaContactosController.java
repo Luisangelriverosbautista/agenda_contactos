d7b89a09c2f44ca467fe9da4adedcd0769952efb
Luis_Ang <158246858+Luisangelriverosbautista@users.noreply.github.com>
2025-05-07 18:14:12 -0600

    act.4 hice el controller

---
+++ src/main/java/com/equipoXD/agenda_contactos/controller/agendaContactosController.java
@@ -0,0 +1,21 @@
+package com.equipoXD.agenda_contactos.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.equipoXD.agenda_contactos.model.agendaContactosModel;
+import com.equipoXD.agenda_contactos.repository.agendaContactosRepository;
+
+@RestController
+@RequestMapping("/contactos")
+public class agendaContactosController {
+    @Autowired
+   private agendaContactosRepository agendaContactosRepository;
+   @GetMapping("/traerContactos")
+   public List <agendaContactosModel> traerContactos() {
+       return agendaContactosRepository.findAll();
+    }
+}
